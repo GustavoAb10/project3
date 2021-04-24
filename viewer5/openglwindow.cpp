8b004b19292026826d3e701615b46f61e4745d13
GustavoAb10 <57842244+GustavoAb10@users.noreply.github.com>
2021-04-24 14:07:19 -0300

    Add files via upload

---
+++ viewer5/openglwindow.cpp
@@ -0,0 +1,489 @@
+#include "openglwindow.hpp"
+
+#include <fmt/core.h>
+#include <imgui.h>
+#include <tiny_obj_loader.h>
+
+#include <cppitertools/itertools.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtx/fast_trigonometry.hpp>
+#include <glm/gtx/hash.hpp>
+#include <unordered_map>
+
+#include "imfilebrowser.h"
+
+void OpenGLWindow::handleEvent(SDL_Event& ev) {
+  if (ev.type == SDL_KEYDOWN) {
+    if (ev.key.keysym.sym == SDLK_UP || ev.key.keysym.sym == SDLK_w)
+      m_dollySpeed = 1.0f;
+    if (ev.key.keysym.sym == SDLK_DOWN || ev.key.keysym.sym == SDLK_s)
+      m_dollySpeed = -1.0f;
+    if (ev.key.keysym.sym == SDLK_LEFT || ev.key.keysym.sym == SDLK_a)
+      m_panSpeed = -1.0f;
+    if (ev.key.keysym.sym == SDLK_RIGHT || ev.key.keysym.sym == SDLK_d)
+      m_panSpeed = 1.0f;
+    if (ev.key.keysym.sym == SDLK_q) m_truckSpeed = -1.0f;
+    if (ev.key.keysym.sym == SDLK_e) m_truckSpeed = 1.0f;
+  }
+  if (ev.type == SDL_KEYUP) {
+    if ((ev.key.keysym.sym == SDLK_UP || ev.key.keysym.sym == SDLK_w) &&
+        m_dollySpeed > 0)
+      m_dollySpeed = 0.0f;
+    if ((ev.key.keysym.sym == SDLK_DOWN || ev.key.keysym.sym == SDLK_s) &&
+        m_dollySpeed < 0)
+      m_dollySpeed = 0.0f;
+    if ((ev.key.keysym.sym == SDLK_LEFT || ev.key.keysym.sym == SDLK_a) &&
+        m_panSpeed < 0)
+      m_panSpeed = 0.0f;
+    if ((ev.key.keysym.sym == SDLK_RIGHT || ev.key.keysym.sym == SDLK_d) &&
+        m_panSpeed > 0)
+      m_panSpeed = 0.0f;
+    if (ev.key.keysym.sym == SDLK_q && m_truckSpeed < 0) m_truckSpeed = 0.0f;
+    if (ev.key.keysym.sym == SDLK_e && m_truckSpeed > 0) m_truckSpeed = 0.0f;
+  }
+}
+
+void OpenGLWindow::initializeGL() {
+  glClearColor(0, 0, 0, 1);
+  glEnable(GL_DEPTH_TEST);
+
+  // Create programs
+  for (const auto& name : m_shaderNames) {
+    auto path{getAssetsPath() + "shaders/" + name};
+    auto program{createProgramFromFile(path + ".vert", path + ".frag")};
+    m_programs.push_back(program);
+  }
+
+  // Load default model
+  loadModel(getAssetsPath() + "CobbleStones2.obj");
+  m_mappingMode = 3;  // "From mesh" option
+  
+  //Second object
+  m_model2.loadFromFile(getAssetsPath() + "12248_Bird_v1_L2.obj");
+  m_model2.setupVAO(m_programs.at(m_currentProgramIndex));
+
+  //Second object
+  m_model3.loadFromFile(getAssetsPath() + "10602_Rubber_Duck_v1_L3.obj");
+  m_model3.setupVAO(m_programs.at(m_currentProgramIndex));
+
+  resizeGL(getWindowSettings().width, getWindowSettings().height);
+}
+
+void OpenGLWindow::loadModel(std::string_view path) {
+  //m_model.loadDiffuseTexture(getAssetsPath() + "maps/pattern.png");
+  //m_model.loadNormalTexture(getAssetsPath() + "maps/pattern_normal.png");
+  m_model.loadFromFile(path);
+  m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+  m_trianglesToDraw = m_model.getNumTriangles();
+
+  // Use material properties from the loaded model
+  m_Ka = m_model.getKa();
+  m_Kd = m_model.getKd();
+  m_Ks = m_model.getKs();
+  m_shininess = m_model.getShininess();
+}
+
+void OpenGLWindow::paintGL() {
+  update();
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glViewport(0, 0, m_viewportWidth, m_viewportHeight);
+
+  // Use currently selected program
+  const auto program{m_programs.at(m_currentProgramIndex)};
+  glUseProgram(program);
+
+  // Get location of uniform variables
+  GLint viewMatrixLoc{glGetUniformLocation(program, "viewMatrix")};
+  GLint projMatrixLoc{glGetUniformLocation(program, "projMatrix")};
+  GLint modelMatrixLoc{glGetUniformLocation(program, "modelMatrix")};
+  GLint normalMatrixLoc{glGetUniformLocation(program, "normalMatrix")};
+  GLint lightDirLoc{glGetUniformLocation(program, "lightDirWorldSpace")};
+  GLint shininessLoc{glGetUniformLocation(program, "shininess")};
+  GLint IaLoc{glGetUniformLocation(program, "Ia")};
+  GLint IdLoc{glGetUniformLocation(program, "Id")};
+  GLint IsLoc{glGetUniformLocation(program, "Is")};
+  GLint KaLoc{glGetUniformLocation(program, "Ka")};
+  GLint KdLoc{glGetUniformLocation(program, "Kd")};
+  GLint KsLoc{glGetUniformLocation(program, "Ks")};
+  GLint diffuseTexLoc{glGetUniformLocation(program, "diffuseTex")};
+  GLint normalTexLoc{glGetUniformLocation(program, "normalTex")};
+  GLint mappingModeLoc{glGetUniformLocation(program, "mappingMode")};
+
+  // Set uniform variables used by every scene object
+  // Set uniform variables for viewMatrix and projMatrix
+  // These matrices are used for every scene object
+  glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_camera.m_viewMatrix[0][0]);
+  glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_camera.m_projMatrix[0][0]);
+  glUniform1i(diffuseTexLoc, 0);
+  glUniform1i(normalTexLoc, 1);
+  glUniform1i(mappingModeLoc, m_mappingMode);
+
+  auto lightDirRotated{m_lightDir};
+  glUniform4fv(lightDirLoc, 1, &lightDirRotated.x);
+  glUniform4fv(IaLoc, 1, &m_Ia.x);
+  glUniform4fv(IdLoc, 1, &m_Id.x);
+  glUniform4fv(IsLoc, 1, &m_Is.x);
+
+  m_modelMatrix = glm::mat4(1.0);
+    m_modelMatrix = glm::translate(m_modelMatrix, glm::vec3(0.0f, 0.0f, 1.0f));
+    m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(90.0f), glm::vec3(0, 1, 0));
+    m_modelMatrix = glm::scale(m_modelMatrix, glm::vec3(1.0f));
+
+    // Set uniform variables of the current object
+    glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+
+    auto modelViewMatrix{glm::mat3(m_viewMatrix * m_modelMatrix)};
+    glm::mat3 normalMatrix{glm::inverseTranspose(modelViewMatrix)};
+    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+    glUniform1f(shininessLoc, m_shininess);
+    glUniform4fv(KaLoc, 1, &m_Ka.x);
+    glUniform4fv(KdLoc, 1, &m_Kd.x);
+    glUniform4fv(KsLoc, 1, &m_Ks.x);
+
+    m_model.render(m_trianglesToDraw);
+
+  for(float i=0.0f;i<=10.0f;i+=0.5){
+      for(float j=0.0f;j<=10.0f;j+=0.5){
+      m_modelMatrix = glm::translate(m_modelMatrix, glm::vec3(i, 0.0f, j));
+      m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(90.0f), glm::vec3(0, 1, 0));
+      m_modelMatrix = glm::scale(m_modelMatrix, glm::vec3(1.0f));
+
+      // Set uniform variables of the current object
+      glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+      glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+      glUniform1f(shininessLoc, m_shininess);
+      glUniform4fv(KaLoc, 1, &m_Ka.x);
+      glUniform4fv(KdLoc, 1, &m_Kd.x);
+      glUniform4fv(KsLoc, 1, &m_Ks.x);
+
+      m_model.render(m_trianglesToDraw);
+    }
+  };
+
+  m_modelMatrix = glm::translate(m_modelMatrix, glm::vec3(0.0f, 0.0f, 1.5f));
+  m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(90.0f), glm::vec3(0, 1, 0));
+  m_modelMatrix = glm::scale(m_modelMatrix, glm::vec3(1.0f));
+
+  // Set uniform variables of the current object
+  glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+  glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+  glUniform1f(shininessLoc, m_shininess);
+  glUniform4fv(KaLoc, 1, &m_Ka.x);
+  glUniform4fv(KdLoc, 1, &m_Kd.x);
+  glUniform4fv(KsLoc, 1, &m_Ks.x);
+
+  m_model.render(m_trianglesToDraw);
+
+  m_trianglesToDraw = m_model2.getNumTriangles() + m_model.getNumTriangles() ;
+
+
+  m_modelMatrix2 = glm::mat4(1.0);
+  m_modelMatrix2 = glm::translate(m_modelMatrix2, glm::vec3(m_camera.m_eye.x,m_camera.m_eye.y,m_camera.m_eye.z - 0.5f));
+  m_modelMatrix2 = glm::rotate(m_modelMatrix2, glm::radians(270.0f), glm::vec3(1, 0, 0));
+  m_modelMatrix2 = glm::rotate(m_modelMatrix2, glm::radians(180.0f), glm::vec3(0, 0, 1));
+  m_modelMatrix2 = glm::scale(m_modelMatrix2, glm::vec3(0.2f));
+
+  // Set uniform variables of the current object
+  glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix2[0][0]);
+
+  glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+  m_model2.render(m_trianglesToDraw);
+
+  for(float k = 5.5f;k<6.2f;k+=0.3){
+      for(float l = 7.5f;l<8.5f;l+=0.3){
+        m_modelMatrix3 = glm::mat4(1.0);
+        m_modelMatrix3 = glm::translate(m_modelMatrix3, glm::vec3(l,0.1f, -k));
+        m_modelMatrix3 = glm::rotate(m_modelMatrix3, glm::radians(270.0f), glm::vec3(1, 0, 0));
+        m_modelMatrix3 = glm::scale(m_modelMatrix3, glm::vec3(0.1f));
+
+        // Set uniform variables of the current object
+        glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix3[0][0]);
+
+        glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+        m_model3.render(m_trianglesToDraw);
+    }
+  }
+  
+
+  glUseProgram(0);
+}
+
+void OpenGLWindow::renderTeste(){
+  
+}
+
+void OpenGLWindow::paintUI() {
+  abcg::OpenGLWindow::paintUI();
+
+  // File browser for models
+  static ImGui::FileBrowser fileDialogModel;
+  fileDialogModel.SetTitle("Load 3D Model");
+  fileDialogModel.SetTypeFilters({".obj"});
+  fileDialogModel.SetWindowSize(m_viewportWidth * 0.8f,
+                                m_viewportHeight * 0.8f);
+
+  // File browser for textures
+  static ImGui::FileBrowser fileDialogDiffuseMap;
+  fileDialogDiffuseMap.SetTitle("Load Diffuse Map");
+  fileDialogDiffuseMap.SetTypeFilters({".jpg", ".png"});
+  fileDialogDiffuseMap.SetWindowSize(m_viewportWidth * 0.8f,
+                                     m_viewportHeight * 0.8f);
+
+  // File browser for normal maps
+  static ImGui::FileBrowser fileDialogNormalMap;
+  fileDialogNormalMap.SetTitle("Load Normal Map");
+  fileDialogNormalMap.SetTypeFilters({".jpg", ".png"});
+  fileDialogNormalMap.SetWindowSize(m_viewportWidth * 0.8f,
+                                    m_viewportHeight * 0.8f);
+
+// Only in WebGL
+#if defined(__EMSCRIPTEN__)
+  fileDialogModel.SetPwd(getAssetsPath());
+  fileDialogDiffuseMap.SetPwd(getAssetsPath() + "/maps");
+  fileDialogNormalMap.SetPwd(getAssetsPath() + "/maps");
+#endif
+
+  // Create main window widget
+  {
+    auto widgetSize{ImVec2(222, 190)};
+
+    if (!m_model.isUVMapped()) {
+      // Add extra space for static text
+      widgetSize.y += 26;
+    }
+
+    ImGui::SetNextWindowPos(ImVec2(m_viewportWidth - widgetSize.x - 5, 5));
+    ImGui::SetNextWindowSize(widgetSize);
+    auto flags{ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDecoration};
+    ImGui::Begin("Widget window", nullptr, flags);
+
+    // Menu
+    {
+      bool loadModel{};
+      bool loadDiffMap{};
+      bool loadNormalMap{};
+      if (ImGui::BeginMenuBar()) {
+        if (ImGui::BeginMenu("File")) {
+          ImGui::MenuItem("Load 3D Model...", nullptr, &loadModel);
+          ImGui::MenuItem("Load Diffuse Map...", nullptr, &loadDiffMap);
+          ImGui::MenuItem("Load Normal Map...", nullptr, &loadNormalMap);
+          ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+      }
+      if (loadModel) fileDialogModel.Open();
+      if (loadDiffMap) fileDialogDiffuseMap.Open();
+      if (loadNormalMap) fileDialogNormalMap.Open();
+    }
+
+    // Slider will be stretched horizontally
+    ImGui::PushItemWidth(widgetSize.x - 16);
+    ImGui::SliderInt("", &m_trianglesToDraw, 0, m_model.getNumTriangles() + m_model2.getNumTriangles(),
+                     "%d triangles");
+    ImGui::PopItemWidth();
+
+    static bool faceCulling{};
+    ImGui::Checkbox("Back-face culling", &faceCulling);
+
+    if (faceCulling) {
+      glEnable(GL_CULL_FACE);
+    } else {
+      glDisable(GL_CULL_FACE);
+    }
+
+    // CW/CCW combo box
+    {
+      static std::size_t currentIndex{};
+      std::vector<std::string> comboItems{"CCW", "CW"};
+
+      ImGui::PushItemWidth(120);
+      if (ImGui::BeginCombo("Front face",
+                            comboItems.at(currentIndex).c_str())) {
+        for (auto index : iter::range(comboItems.size())) {
+          const bool isSelected{currentIndex == index};
+          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
+            currentIndex = index;
+          if (isSelected) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+      ImGui::PopItemWidth();
+
+      if (currentIndex == 0) {
+        glFrontFace(GL_CCW);
+      } else {
+        glFrontFace(GL_CW);
+      }
+    }
+
+    // Projection combo box
+    {
+      static std::size_t currentIndex{};
+      std::vector<std::string> comboItems{"Perspective", "Orthographic"};
+
+      ImGui::PushItemWidth(120);
+      if (ImGui::BeginCombo("Projection",
+                            comboItems.at(currentIndex).c_str())) {
+        for (auto index : iter::range(comboItems.size())) {
+          const bool isSelected{currentIndex == index};
+          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
+            currentIndex = index;
+          if (isSelected) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+      ImGui::PopItemWidth();
+
+      auto aspect{static_cast<float>(m_viewportWidth) /
+                  static_cast<float>(m_viewportHeight)};
+      if (currentIndex == 0) {
+        m_projMatrix =
+            glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
+
+      } else {
+        m_projMatrix =
+            glm::ortho(-1.0f * aspect, 1.0f * aspect, -1.0f, 1.0f, 0.1f, 5.0f);
+      }
+    }
+
+    // Shader combo box
+    {
+      static std::size_t currentIndex{};
+
+      ImGui::PushItemWidth(120);
+      if (ImGui::BeginCombo("Shader", m_shaderNames.at(currentIndex))) {
+        for (auto index : iter::range(m_shaderNames.size())) {
+          const bool isSelected{currentIndex == index};
+          if (ImGui::Selectable(m_shaderNames.at(index), isSelected))
+            currentIndex = index;
+          if (isSelected) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+      ImGui::PopItemWidth();
+
+      // Set up VAO if shader program has changed
+      if (static_cast<int>(currentIndex) != m_currentProgramIndex) {
+        m_currentProgramIndex = currentIndex;
+        m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+      }
+    }
+
+    if (!m_model.isUVMapped()) {
+      ImGui::TextColored(ImVec4(1, 1, 0, 1), "Mesh has no UV coords.");
+    }
+
+    // UV mapping box
+    {
+      std::vector<std::string> comboItems{"Triplanar", "Cylindrical",
+                                          "Spherical"};
+
+      if (m_model.isUVMapped()) comboItems.emplace_back("From mesh");
+
+      ImGui::PushItemWidth(120);
+      if (ImGui::BeginCombo("UV mapping",
+                            comboItems.at(m_mappingMode).c_str())) {
+        for (auto index : iter::range(comboItems.size())) {
+          const bool isSelected{m_mappingMode == static_cast<int>(index)};
+          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
+            m_mappingMode = index;
+          if (isSelected) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+      ImGui::PopItemWidth();
+    }
+
+    ImGui::End();
+  }
+
+  // Create window for light sources
+  if (m_currentProgramIndex < 4) {
+    auto widgetSize{ImVec2(222, 244)};
+    ImGui::SetNextWindowPos(ImVec2(m_viewportWidth - widgetSize.x - 5,
+                                   m_viewportHeight - widgetSize.y - 5));
+    ImGui::SetNextWindowSize(widgetSize);
+    ImGui::Begin(" ", nullptr, ImGuiWindowFlags_NoDecoration);
+
+    ImGui::Text("Light properties");
+
+    // Slider to control light properties
+    ImGui::PushItemWidth(widgetSize.x - 36);
+    ImGui::ColorEdit3("Ia", &m_Ia.x, ImGuiColorEditFlags_Float);
+    ImGui::ColorEdit3("Id", &m_Id.x, ImGuiColorEditFlags_Float);
+    ImGui::ColorEdit3("Is", &m_Is.x, ImGuiColorEditFlags_Float);
+    ImGui::PopItemWidth();
+
+    ImGui::Spacing();
+
+    ImGui::Text("Material properties");
+
+    // Slider to control material properties
+    ImGui::PushItemWidth(widgetSize.x - 36);
+    ImGui::ColorEdit3("Ka", &m_Ka.x, ImGuiColorEditFlags_Float);
+    ImGui::ColorEdit3("Kd", &m_Kd.x, ImGuiColorEditFlags_Float);
+    ImGui::ColorEdit3("Ks", &m_Ks.x, ImGuiColorEditFlags_Float);
+    ImGui::PopItemWidth();
+
+    // Slider to control the specular shininess
+    ImGui::PushItemWidth(widgetSize.x - 16);
+    ImGui::SliderFloat("", &m_shininess, 0.0f, 500.0f, "shininess: %.1f");
+    ImGui::PopItemWidth();
+
+    ImGui::End();
+  }
+
+  fileDialogModel.Display();
+  if (fileDialogModel.HasSelected()) {
+    loadModel(fileDialogModel.GetSelected().string());
+    fileDialogModel.ClearSelected();
+
+    if (m_model.isUVMapped()) {
+      // Use mesh texture coordinates if available...
+      m_mappingMode = 3;
+    } else {
+      // ...or triplanar mapping otherwise
+      m_mappingMode = 0;
+    }
+  }
+
+  fileDialogDiffuseMap.Display();
+  if (fileDialogDiffuseMap.HasSelected()) {
+    m_model.loadDiffuseTexture(fileDialogDiffuseMap.GetSelected().string());
+    fileDialogDiffuseMap.ClearSelected();
+  }
+
+  fileDialogNormalMap.Display();
+  if (fileDialogNormalMap.HasSelected()) {
+    m_model.loadNormalTexture(fileDialogNormalMap.GetSelected().string());
+    fileDialogNormalMap.ClearSelected();
+  }
+}
+
+void OpenGLWindow::resizeGL(int width, int height) {
+  m_viewportWidth = width;
+  m_viewportHeight = height;
+
+  m_camera.computeProjectionMatrix(width, height);
+}
+
+void OpenGLWindow::terminateGL() {
+  for (const auto& program : m_programs) {
+    glDeleteProgram(program);
+  }
+}
+
+void OpenGLWindow::update() {
+  float deltaTime{static_cast<float>(getDeltaTime())};
+
+  // Update LookAt camera
+  m_camera.dolly(m_dollySpeed * deltaTime);
+  m_camera.truck(m_truckSpeed * deltaTime);
+  m_camera.pan(m_panSpeed * deltaTime);
+}
